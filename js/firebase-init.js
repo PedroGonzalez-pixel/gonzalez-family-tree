ee194efa67e6f20ad0d8e5d7cdb3c6ed5df35c4e
PedroGonzalez-pixel <jean.pierre.gonzalez@gmail.com>
2026-04-01 15:02:35 +0200

    Create firebase-init.js

---
+++ js/firebase-init.js
@@ -0,0 +1,15 @@
+// Firebase configuration générée automatiquement
+const firebaseConfig = {
+  apiKey: "AIzaSyAKRVr5f82dRqBAGTbfePCxIidUtaRKhHo",
+  authDomain: "gonzalezfamilytree-657b9.firebaseapp.com",
+  projectId: "gonzalezfamilytree-657b9",
+  storageBucket: "gonzalezfamilytree-657b9.firebasestorage.app",
+  messagingSenderId: "400439887654",
+  appId: "1:400439887654:web:b45c1ea099aca1a14b23e7"
+};
+
+// Initialiser Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
